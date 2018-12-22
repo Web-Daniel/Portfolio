e10dcacd6a583e6b59d7ddf94a4c450df324a39d
Web-Daniel <daniilmcmxciii@gmail.com>
2018-12-23 04:48:38 +0500

    add fix myFunc

--- js/func.js
+++ js/func.js
@@ -10,17 +10,6 @@ var colors = ['#FF0000','#FFA500','#FFFF00','#008000','#00FFFF','#1E90FF','#8000
 var backgrounds = ['#00CC00','#1047A9','#7109AA','#A00000','#FF7400','#FF9C0C','#FFF100'];
 var i = 0;
 var j = 0;
-
-var setColors = function() {
-    setInterval(window.document.getElementById('emailString').style.color = colors[i], 1000);
-    if (i >= colors.length) {
-        return i = 0;
-    } else {
-        return i++;
-    }
-};
-setInterval(setColors, 1000);
-
 /*
 var setBackgrounds = function() {
     setInterval(window.document.getElementById('emailString').style.background = backgrounds[j], 1000);
@@ -31,3 +20,27 @@ var setBackgrounds = function() {
     }
 };
 setInterval(setBackgrounds, 1000); */
+
+var setColors = setInterval(function() {
+    setInterval(window.document.getElementById('emailString').style.color = colors[i], 1000);
+    if (i >= colors.length) {
+        return i = 0;
+    } else {
+        return i++;
+    }
+}, 1000);
+setTimeout(function(){
+    clearInterval(setColors);
+}, 58000);
+
+var setBackgrounds = setInterval(function() {
+    setInterval(window.document.getElementById('emailString').style.background = backgrounds[j], 1000);
+    if (j >= backgrounds.length) {
+        return j = 0;
+    } else {
+        return j++;
+    }
+}, 1000);
+setTimeout(function(){
+    clearInterval(setBackgrounds);
+}, 58000);
