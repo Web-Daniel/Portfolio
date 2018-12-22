22266788bb8fa75562c6350fb6ffa5fd6f5e23da
Web-Daniel <daniilmcmxciii@gmail.com>
2018-12-23 04:05:41 +0500

    add myFunc

--- js/func.js
+++ js/func.js
@@ -1,5 +1,3 @@
-//document.getElementById('btnCollapse').innerHTML = 'Дополнительно...(свернуть)';
-
 $('#btnCollapse').click(function () {
     if (($('#collapseExample')).hasClass('show') == false) {
         document.getElementById('btnCollapse').innerHTML = 'Дополнительно...(свернуть)';
@@ -7,3 +5,27 @@ $('#btnCollapse').click(function () {
         document.getElementById('btnCollapse').innerHTML = 'Дополнительно...(развернуть)';
     }
 });
+
+var colors = ['#FF0000','#FFA500','#FFFF00','#008000','#00FFFF','#1E90FF','#8000ff'];
+var backgrounds = ['#00CC00','#1047A9','#7109AA','#A00000','#FF7400','#FF9C0C','#FFF100'];
+var i = 0;
+
+var setColors = function() {
+    setInterval(document.getElementById('emailString').style.color = colors[i], 1000);
+    if (i >= colors.length) {
+        return i = 0;
+    } else {
+        return i++;
+    }
+};
+setInterval(setColors, 1000);
+
+var setBackgrounds = function() {
+    setInterval(document.getElementById('emailString').style.background = backgrounds[i], 1000);
+    if (i >= backgrounds.length) {
+        return i = 0;
+    } else {
+        return i++;
+    }
+};
+setInterval(setBackgrounds, 1000);
