7d82c04dcd2456e5d8f8e8cb8c5f9d3f72fb550a
Web-Daniel <daniilmcmxciii@gmail.com>
2018-12-23 04:20:33 +0500

    fix MyFunc

--- js/func.js
+++ js/func.js
@@ -9,9 +9,10 @@ $('#btnCollapse').click(function () {
 var colors = ['#FF0000','#FFA500','#FFFF00','#008000','#00FFFF','#1E90FF','#8000ff'];
 var backgrounds = ['#00CC00','#1047A9','#7109AA','#A00000','#FF7400','#FF9C0C','#FFF100'];
 var i = 0;
+var j = 0;
 
 var setColors = function() {
-    setInterval(document.getElementById('emailString').style.color = colors[i], 1000);
+    setInterval(window.document.getElementById('emailString').style.color = colors[i], 1000);
     if (i >= colors.length) {
         return i = 0;
     } else {
@@ -20,12 +21,13 @@ var setColors = function() {
 };
 setInterval(setColors, 1000);
 
+/*
 var setBackgrounds = function() {
-    setInterval(document.getElementById('emailString').style.background = backgrounds[i], 1000);
-    if (i >= backgrounds.length) {
-        return i = 0;
+    setInterval(window.document.getElementById('emailString').style.background = backgrounds[j], 1000);
+    if (j >= backgrounds.length) {
+        return j = 0;
     } else {
-        return i++;
+        return j++;
     }
 };
-setInterval(setBackgrounds, 1000);
+setInterval(setBackgrounds, 1000); */
