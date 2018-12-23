87e75085314142f3308d0fc9f55ee77ffeab050c
Web-Daniel <daniilmcmxciii@gmail.com>
2018-12-23 15:57:45 +0500

    Fix myFuncJS

--- js/func.js
+++ js/func.js
@@ -8,19 +8,10 @@ $('#btnCollapse').click(function () {
 
 var colors = ['#FF0000','#FFA500','#FFFF00','#008000','#00FFFF','#1E90FF','#8000ff'];
 var backgrounds = ['#00CC00','#1047A9','#7109AA','#A00000','#FF7400','#FF9C0C','#FFF100'];
-var i = 0;
-var j = 0;
-/*
-var setBackgrounds = function() {
-    setInterval(window.document.getElementById('emailString').style.background = backgrounds[j], 1000);
-    if (j >= backgrounds.length) {
-        return j = 0;
-    } else {
-        return j++;
-    }
-};
-setInterval(setBackgrounds, 1000); */
+var i = -1;
+var timerId;
 
+/*
 var setColors = setInterval(function() {
     setInterval((document.getElementById('emailString').style.color = colors[i]), 1000);
     if (i >= colors.length) {
@@ -43,4 +34,20 @@ var setBackgrounds = setInterval(function() {
 }, 1000);
 setTimeout(function(){
     clearInterval(setBackgrounds);
-}, 58000);
+}, 58000); */
+
+
+var setColors = setTimeout(function tick() {
+    timerId = setTimeout(function tick() {
+        document.getElementById('emailString').style.color = colors[i];
+        document.getElementById('emailString').style.background = backgrounds[i];
+        timerId = setTimeout(tick, 1000);
+      }, 1000);
+    if (i >= colors.length) {
+        setColors = setTimeout(tick, 1000);
+        return i = 0;
+    } else {
+        setColors = setTimeout(tick, 1000);
+        return i++;
+    };
+}, 1000);
